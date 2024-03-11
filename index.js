4d8dfa8205afa7894dfa62b416348575cb2a1699
Creantics <domsendsmail@gmail.com>
2024-03-11 16:43:47 -0400

    Added text boxes for less/more than allowed number

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const userInput = document.getElementById('userInput');
 const paragraphValid = document.getElementById('paragraphValid');
+const validLess = document.getElementById('resultSquareLess');
+const validMore = document.getElementById('resultSquareMore');
+const validEmpty = document.getElementById('resultSquareNotValid');
+
 let convertButton = document.getElementById('convertButton');
+let numberToConvert = "";
 
 console.log(userInput.innerHTML);
 
-function convert() {
-    paragraphValid.innerHTML = userInput.value;
+function convertLess() {
+    numberToConvert = userInput.value;
+    if (numberToConvert <= 0 && numberToConvert) {
+        validLess.classList.remove('hidden');
+        validMore.classList.add('hidden');
+    } 
+}
+
+function convertMore() {
+    numberToConvert = userInput.value;
+    if (numberToConvert > 3999) {
+        validMore.classList.remove('hidden');
+        validLess.classList.add('hidden');
+    } 
 }
