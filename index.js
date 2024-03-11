18567a6c380834bf38e74a0a122de9069493bbeb
Creantics <domsendsmail@gmail.com>
2024-03-11 13:07:03 -0400

    Add userinput functionality

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const userInput = document.getElementById('userInput');
 const paragraphValid = document.getElementById('paragraphValid');
+let convertButton = document.getElementById('convertButton');
 
-console.log(paragraphValid.innerHTML);
+console.log(userInput.innerHTML);
+
+function convert() {
+    paragraphValid.innerHTML = userInput.value;
+}
